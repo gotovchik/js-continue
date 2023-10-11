c6daea6c4cb5f578422879db7f94d46d411a7a61
Игорь Готовчик <gotovchik.igor@yandex.ru>
2023-10-11 09:26:06 +0300

    task1+task2 done

--- functional_js/homework/script.js
+++ functional_js/homework/script.js
@@ -4,11 +4,40 @@
 // найти минимальное число в массиве, решение задание должно состоять из одной
 // строки кода.
 
+const arr = [1, 5, 7, 9];
+console.log(Math.min(...arr));
+
 // 2) Напишите функцию createCounter, которая создает счетчик и возвращает объект
 // с двумя методами: increment и decrement. Метод increment должен увеличивать
 // значение счетчика на 1, а метод decrement должен уменьшать значение счетчика
 // на 1. Значение счетчика должно быть доступно только через методы объекта,
 // а не напрямую.
+
+function createCounter() {
+  let count = 0;
+
+  function counter() {
+    return count;
+  }
+
+  counter.increment = () => {
+    count++;
+  };
+
+  counter.decrement = () => {
+    count--;
+  };
+
+  return counter;
+}
+
+const counter = createCounter();
+console.log(counter());
+counter.increment();
+counter.increment();
+console.log(counter());
+counter.decrement();
+console.log(counter());
 
 // 3) Дополнительное задание, выполняем только если проходили работу с DOM.
 // Напишите рекурсивную функцию findElementByClass, которая принимает корневой
